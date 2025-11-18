1efd8ffcad8896d4950778156da6fc91672c12c4
dnezcom <dnezcom@gmail.com>
2025-11-18 02:36:51 +0000

    update/Database Migration Tutorial

---
+++ migrations/versions/18a93d4f3bb1_initial_baseline_migration.py
@@ -0,0 +1,24 @@
+"""Initial baseline migration
+
+Revision ID: 18a93d4f3bb1
+Revises: 
+Create Date: 2025-11-18 02:28:38.684105
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '18a93d4f3bb1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
